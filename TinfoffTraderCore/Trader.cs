f141fe5b980dd2e94088b86734b5c4b634db719c
Yaroslav Ermilov <ya.ermilov@gmail.com>
2020-01-11 18:36:47 +0300

    Инициализация репозитория

---
+++ TinfoffTraderCore/Trader.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Tinkoff.Trading.OpenApi.Network;
+using TinkoffTraderCore.Modules.Instruments;
+using TinkoffTraderCore.Modules.Positions;
+
+namespace TinkoffTraderCore
+{
+    public class Trader
+    {
+        private readonly string _token;
+        private readonly bool _useSandbox;
+
+        #region .ctor
+
+        public Trader(string token, bool useSandbox)
+        {
+            _token = token;
+            _useSandbox = useSandbox;
+        }
+
+        #endregion
+    }
+}
---
+++ TinkoffTrader/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using TinkoffTrader.ViewModels;
+using TinkoffTraderCore;
+
+namespace TinkoffTrader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
