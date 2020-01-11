39205fe53e88d010158dcd8b560a0bf6fad90089
Yaroslav Ermilov <ya.ermilov@gmail.com>
2020-01-11 18:44:05 +0300

    GUI: добавлена MainViewModel

---
+++ TinkoffTrader/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using TinkoffTraderCore;
+
+namespace TinkoffTrader.ViewModels
+{
+    internal class MainViewModel : ABaseViewModel
+
+        /// <summary>
+        /// True, если включен режим песочницы
+        /// </summary>
+        public bool IsSandboxMode { get => Get<bool>(true); set => Set(value); }
+
+        /// <summary>
+        /// Текст вывода
+        /// </summary>
+        public string Output { get => Get<string>(); set => Set(value); }
+
+        public void WriteLine(string text = null)
+        {
+            if (text != null)
+            {
+                Output += text;
+            }
+
+            Output += "\n";
+        }
+
+        public ICommand Start => new BaseCommand(StartAction);
+
+        private async void StartAction()
+        {
+        }
+    }
+}
